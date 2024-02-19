4b02e8e0a66a06b1cff86db567ad4d65f6102d4e
Ashish75585 <otherside75585@gmail.com>
2024-02-19 20:21:12 +0530

    initial commit

---
+++ signature.py
@@ -0,0 +1,45 @@
+import cv2
+import os
+from skimage.metrics import structural_similarity as ssim
+from tkinter import messagebox
+import time
+
+# TODO add contour detection for enhanced accuracy
+
+
+def check_path(path):
+    if os.path.exists(path):
+        return True
+    else:
+        return False
+
+
+def match(path1, path2):
+    if not check_path(path1) or not check_path(path2):
+            messagebox.showerror(title="Invalid Path", message="The paths you provided does not exists. "
+                                                               "Please Provide valid path")
+            return -1
+    # read the images
+    img1 = cv2.imread(path1)
+    img2 = cv2.imread(path2)
+    # turn images to grayscale
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    # resize images for comparison
+    img1 = cv2.resize(img1, (300, 300))
+    img2 = cv2.resize(img2, (300, 300))
+    # display both images
+    cv2.imshow("Image1", img1)
+    cv2.imshow("Image2", img2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    similarity_value = "{:.2f}".format(ssim(img1, img2)*100)
+    # print("answer is ", float(similarity_value),
+    #       "type=", type(similarity_value))
+    return float(similarity_value)
+
+
+# ans = match("D:\\Code\\Git stuff\\Signature-Matching\\assets\\1.png",
+#             "D:\\Code\\Git stuff\\Signature-Matching\\assets\\3.png")
+# print(ans)
+# print(type(ans))
